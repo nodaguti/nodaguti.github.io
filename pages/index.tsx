5f4e400ca264d044e47b6c2ed159745ebc63673b
nodaguti <nodaguti@gmail.com>
2021-05-30 10:47:56 +0900

    Add client-side sre to tech interests

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,6 +96,7 @@ const Home = () => (
           <h1 className="section__title">Tech Interests</h1>
           <ul>
             <li>Web Performance</li>
+            <li>Client-side Site Reliability Engineering</li>
             <li>Build and Coding Infrastructure (transpiling, formatting, linting, etc.)</li>
             <li>Story-driven and Data-driven Development</li>
           </ul>
