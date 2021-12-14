739b1453bfc80a650cbe44b7c4dba954a1eb961e
nodaguti <nodaguti@users.noreply.github.com>
2021-12-14 13:41:30 +0000

    Automated deployment: Tue Dec 14 13:41:30 UTC 2021 9e73cdcd23c40d098ca867aadf35a7553a73756b

--- _next/static/UH_CPBPRWmF_CrgibThTL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-68da9edf10239c8b7ae0.js"],"/_error":["static/chunks/pages/_error-d799e3a96772416696b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
