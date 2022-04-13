0c080351909fc270e69ab1907e19fa21d40f72ed
nodaguti <nodaguti@gmail.com>
2022-04-13 15:22:35 +0900

    Change conference name from CA BASE NEXT to CA BASE NEXT 2021

--- pages/index.tsx
+++ pages/index.tsx
@@ -178,8 +178,8 @@ const Home = (
               ABEMA Webブラウザ版をより高速で高信頼にするために (Towards more performant and reliable ABEMA)
               </ExternalLink>
               ,&nbsp;
-              <ExternalLink href="https://ca-base-next.cyberagent.co.jp/">
-                CA BASE NEXT
+              <ExternalLink href="https://ca-base-next.cyberagent.co.jp/2021">
+                CA BASE NEXT 2021
               </ExternalLink>
               , 28 May 2021.
             </li>
